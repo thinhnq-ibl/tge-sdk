896a9a071ba8e59577244542248729d4e22d8047
thinhnguyen <thinhnq@blockchainlabs.asia>
2025-01-08 17:01:45 +0700

    change file name

---
+++ src/ApiService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Thinhnguyen\TgeSdk;
+use GuzzleHttp\Client;
+
+class ApiService
+{
+    private $client;
+
+    public function __construct($baseUri)
+    {
+        $this->client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $baseUri,
+            // You can set any number of default request options.
+            'timeout' => 2.0,
+        ]);
+
+    }
+
+    public function getData()
+    {
+        $response = $this->client->get("/get");
+        return $response->getBody();
+    }
+}
